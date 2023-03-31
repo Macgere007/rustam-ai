cbc2dd701c5b8a3fc186c2f6f1b1047b760045eb
Macgere 007 <mrfelly09@gmail.com>
2023-04-01 00:07:00 +0700

    fixing

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -90,7 +90,7 @@ const CreatePost = () => {
             labelName="Your Name"
             type="text"
             name="name"
-            placeholder="Ex., Rustam"
+            placeholder="Rustam"
             value={form.name}
             handleChange={handleChange}
           />
@@ -99,7 +99,7 @@ const CreatePost = () => {
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
